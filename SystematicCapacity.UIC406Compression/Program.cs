1fe8c2c67f7ab9c5b5e33e85c9dde4fe63c244d1
Liao Zhengwen <zwliao@bjtu.edu.cn>
2020-10-24 18:47:22 +0800

    Initialize program
    generate a C# program on .Net Core 3.1

---
+++ SystematicCapacity.UIC406Compression/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SystematicCapacity.Core;
+
+namespace SystematicCapacity.UIC406Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalDataRepository dataRepository = new GlobalDataRepository();
+
+
+            Console.WriteLine("UIC 406 timetable compression.");
+        }
+    }
+}
